39e26156265acbd2c3e7c8d75ee7002c3ce991a1
JohnnyRamirezSancho <info@johnnyramirez.es>
2022-11-29 19:13:48 +0100

    String Exercises

--- src/fundamentals-ex/stringExercises.js
+++ src/fundamentals-ex/stringExercises.js
@@ -1,49 +1,58 @@
-export function returnHello()
-{
-
+export function returnHello(word) {
+    return word;
 }
 
-export function returnAString()
-{
-    /* Completa la function */
-    
+export function returnAString() {
+    return 'Hello World';
 }
 
-export function concatenateStrings()
-{
+export function concatenateStrings() {
     /* Completa la function */
     const firstWord = 'I';
     const secondWord = 'Love';
-    
+    const thirdWord = 'JS';
+    const space = " ";
+    const result = firstWord + space + secondWord + space + thirdWord;
+    return result;
+
 }
 
-export function giveOneLetter()
-{
+export function giveOneLetter(searchLetter) {
     /* Completa la function */
     const dog = 'dog';
+    const foundLetter = dog.indexOf(searchLetter);
+    if (foundLetter >= 0) {
+        return searchLetter;
+    }
+    return false;
+
 }
 
-export function giveTheIndexOfWord()
-{
+export function giveTheIndexOfWord() {
     const wordToSearch = 'Butterfly';
     const phrase = "Once upon a time!!!";
-    
+    const result = phrase.indexOf(wordToSearch);
+    return result;
+
 }
 
-export function replaceFishWords()
-{
+export function replaceFishWords(replacement) {
     let phrase = 'Give a Man a Fish';
-    
+    phrase = phrase.replace("Fish", replacement);
+    return phrase;
+
 }
 
-export function giveAllLettersInUppercase()
-{
+export function giveAllLettersInUppercase() {
     let phrase = 'Ride Him, Cowboy!';
-    
+    const result = phrase.toUpperCase();
+    return result;
 }
 
-export function removeWhiteSpaces()
-{
+export function removeWhiteSpaces() {
     let phrase = '    Hands Down    ';
-    
+    const result = phrase.trim();
+    return result;
+
+
 }
--- tests/fundamentals-ex/stringExercises.test.js
+++ tests/fundamentals-ex/stringExercises.test.js
@@ -1,10 +1,10 @@
-import { returnAString, returnHello } from "../../src/fundamentals-ex/stringExercises";
+import { concatenateStrings, giveAllLettersInUppercase, giveOneLetter, giveTheIndexOfWord, removeWhiteSpaces, replaceFishWords, returnAString, returnHello } from "../../src/fundamentals-ex/stringExercises";
 
 describe('Variables can be a string', () => {
     test.skip('should say Hello', () => {
-        /* agrega la línea que falta */
         const word = "Hello";
-        expect(returnHello()).toBe('Hello');
+        const result = returnHello(word);
+        expect(result).toBe('Hello');
     });
 
     test.skip('should return Hello World', () => {
@@ -15,37 +15,40 @@ describe('Variables can be a string', () => {
 
     test.skip('should be return I Love JS', () => {
         /* Añade las líneas necesarias al test / Modifica la function concatenateStrings */
-
-        expect().toBe('I Love JS');
+        
+        const result = concatenateStrings();
+        expect(result).toBe('I Love JS');
     });
 
-    test.skip('should return letter g', () => {
+    test('should return letter g', () => {
         /* Añade las líneas necesarias al test / Modifica la function giveOneLetter para que devuelva la letra g de la variable configurada */
-        
-        expect().toBe('g');
+        const letter = "g";
+        const searchLetter = giveOneLetter(letter);
+        expect(searchLetter).toBe('g');
     });
 
     test.skip('should return -1', () => {
         /* Añade las líneas necesarias al test / Modifica la function giveTheIndexOfWord */
-
-        expect().toEqual(-1);
+        const result = giveTheIndexOfWord();
+        expect(result).toEqual(-1);
     });
 
     test.skip('should return "Give a Man a Pussy Cat" ', () => {
         /* Añade las líneas necesarias al test / Modifica la function replaceFishWords */
-
-        expect().toBe('Give a Man a Pussy Cat');
+        const replacement = "Pussy Cat";
+        const result = replaceFishWords(replacement);
+        expect(result).toBe('Give a Man a Pussy Cat');
     });
 
     test.skip('should return "RIDE HIM, COWBOY!"', () => {
         /* Añade las líneas necesarias al test / Modifica la function giveAllLettersInUppercase */
-        
-        expect().toBe('RIDE HIM, COWBOY');
+        const result = giveAllLettersInUppercase();
+        expect(result).toBe('RIDE HIM, COWBOY!');
     });
 
     test.skip('should return "Hands Down"', () => {
         /* Añade las líneas necesarias al test y modifica la function removeWhiteSpace */
-
-        expect().toBe('Hands Down');
+        const result = removeWhiteSpaces();
+        expect(result).toBe('Hands Down');
     });
 });
